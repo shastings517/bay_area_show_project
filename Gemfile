935228459998914862eefb6bf295f2eef4f62b10
Mark Marsella <mmarsella114@g.rwu.edu>
2015-11-03 15:27:48 -0800

    add api call for date and location shows, add index loop to render shows

--- Gemfile
+++ Gemfile
@@ -38,6 +38,11 @@ gem "acts_as_follower"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+
+# API CALLS
+gem "typhoeus"
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,6 +1,11 @@
 class ShowsController < ApplicationController
   def index
-  @shows = Show.all
+
+    @response = Typhoeus::Request.new("http://api.bandsintown.com/events/search.json?location=San+Francisco,CA&date=2015-11-03,2015-11-07&radius=10&app_id=777").run
+    @response_body = JSON.parse(@response.response_body)
+
+    # All the user-defined shows in the db
+    @shows = Show.all
   end
 
   def new
@@ -9,13 +14,13 @@ class ShowsController < ApplicationController
   def edit
   end
 
-    def show
-  	  begin
-    @show = Show.find params[:id]
-  rescue
-    flash[:error] = "No Shows Found"
-    redirect :action => :index
- end
-end
+  def show
+    begin
+      @show = Show.find params[:id]
+    rescue
+      flash[:error] = "No Shows Found"
+      redirect :action => :index
+   end
+  end
 end
 
