1f323966c317eca3be314328e90707f265170815
Mark Marsella <mmarsella114@g.rwu.edu>
2015-11-02 17:00:51 -0800

    user_show specs pass

--- spec/models/user_show_spec.rb
+++ spec/models/user_show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe UserShow do 
 
-  it {is_expected.to belong_to :users}
-  it {is_expected.to belong_to :shows}
+  it {is_expected.to belong_to :user}
+  it {is_expected.to belong_to :show}
 
 end
