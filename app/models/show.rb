e53a4fd8dcc150c936ef71ed17c751040749113f
Mark Marsella <mmarsella114@g.rwu.edu>
2015-11-02 16:55:44 -0800

    show specs pass

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,14 @@
 class Show < ActiveRecord::Base
+
+  # VALIDATIONS
+
+  validates :title, presence: true
+  validates :venue, presence: true
+  validates :genre, presence: true
+  validates :showdate, presence: true
+
+  validates :title, uniqueness: true
+
 	has_many :users, through: :user_shows
 	has_many :user_shows
 end
