418de63eb233408766e0faeb6ebc1c7d6a5fa1b9
donte <digitalartcrew@gmail.com>
2015-11-04 03:29:58 -0800

    Added Key

---
+++ config/initializers/koala.rb
@@ -0,0 +1,8 @@
+# # config/initializers/koala.rb
+# require 'koala'
+
+# Koala.configure do |config|
+#   config.graph_server = 'my-graph-mock.mysite.com'
+#   # other common options are `rest_server` and `dialog_host`
+#   # see lib/koala/http_service.rb
+# end
