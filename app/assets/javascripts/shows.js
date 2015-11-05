1a43ddde1fff55cee13c8f1947a52c20d8fd8919
Samuel Hastings <shastings517@gmail.com>
2015-11-04 17:50:27 -0800

    adding styling

---
+++ app/assets/javascripts/shows.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  
+
+  // $(document.body).on('hidden.bs.modal', function () {
+  //   $('#myModal').removeData('bs.modal');
+  // });
+
+  // $('body').on('hidden.bs.modal', '#editModal', function () {
+  //   $(this).removeData('bs.modal');
+  //   $(this).find('.modal-body').html('');
+  // });
+
+  // $(".attendBtn").on('click', function(){
+  //   var data = { show: { attendance: attendance } };
+  //       $.ajax({
+  //         type: 'post',
+  //         url: '/notes',
+  //         dataType: 'json',
+  //         data: data
+
+
+  // });
+
+
+
+});
