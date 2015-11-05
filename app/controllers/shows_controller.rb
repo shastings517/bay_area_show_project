91be983b116ebec1d6f098d7a819fb6d3552cdb4
donte <digitalartcrew@gmail.com>
2015-11-05 13:05:29 -0800

    Single User sign on working in development

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,7 +1,9 @@
 class ShowsController < ApplicationController
+
   before_action :find_show, only: [:show,:edit,:update,:destroy]
 
   def index
+
     n = 2
     t = Time.zone.now
     today = t.year.to_s + "-" + t.month.to_s.rjust(n, "0") + "-" + t.day.to_s.rjust(n, "0") 
@@ -9,116 +11,36 @@ class ShowsController < ApplicationController
     endDate = threeDays.year.to_s + "-" + threeDays.month.to_s.rjust(n, "0") + "-" + threeDays.day.to_s.rjust(n, "0") 
 
     # API REQUEST
-    @response = Typhoeus::Request.new("http://api.bandsintown.com/events/search.json?location=San+Francisco,CA&date=#{today},#{endDate}&radius=10&app_id=777").run
+    @response = Typhoeus::Request.new("http://api.bandsintown.com/events/search.json?location=San+Francisco,CA&date="+today+","+endDate+"&radius=10&app_id=777").run
 
     # API RESPONSE
     @response_body = JSON.parse(@response.response_body)
 
-    puts @response_body
-
-    # loop through the response
-    # Make a new Show document per api response
-    # store in the db
-    # delete shows that have "expired"?
-
-    @response_body.each do |show|
-
-      # date --> gives back a DateTime object
-      date = DateTime.parse(show["datetime"])
-
-      unless show["on_sale_datetime"] == nil
-      ticketDate = DateTime.parse(show["on_sale_datetime"])
-      end
-
-      artist = show["artists"][0]["name"]
-
-      artistArr = artist.split(" ")
-
-      apiString = ""
-
-      if artistArr.length > 1
-        artistArr.each do |word|
-          if word == artistArr.last
-            apiString += word
-          else
-            apiString += word + "%20"
-          end
-        end
-      else
-        apiString = artist
-      end
-
-        @artist_response = Typhoeus::Request.new("http://api.bandsintown.com/artists/#{apiString}.json?api_version=2.0&app_id=777").run
-        @artist_body = JSON.parse(@artist_response.response_body)
-        image = @artist_body["image_url"]
-
-      # API CALL FOR ARTIST IMAGE
-      # @artist_response = Typhoeus::Request.new("http://api.bandsintown.com/artists/Beach%20House.json?api_version=2.0&app_id=777").run
-      # @artist_body = JSON.parse(@artist_response.response_body)
-      # image = @artist_body["image_url"]
-      ##########################################################
-
-      venue = show["venue"]["name"]
-      latitude = show["venue"]["latitude"]
-      longitude = show["venue"]["longitude"]
-      tickets = show["ticket_url"]
-
-      show = Show.create(
-        title: artist,
-        venue: venue,
-        image_url: image,
-        attendance: 0,
-        latitude: latitude, 
-        longitude: longitude,
-        showdate: date,
-        saledate: ticketDate,
-        showtime: date
-        )
-    end
-
-    # IMAGE LOGIC
-    # If image_url is present, DO NOT MAKE API CALL, if not then make API call.
-    # Which means show = Show.new() and then .save() after api call
     # All the user-defined shows in the db
-    date = Date.today
 
-    # Find all shows from today til 3 days from now in DB
-    oneDay = date.tomorrow
-    twoDays = date.tomorrow.tomorrow
-    threeDays = date.tomorrow.tomorrow.tomorrow
-    @shows = Show.where('showdate': date)
-    @tomorrow = Show.where('showdate': oneDay)
-    @dayAfter = Show.where('showdate': twoDays)
-    @dayAfterThat = Show.where('showdate': threeDays)
+     # ####  CURRENTLY NOT FILTERING OUT ANY SHOWS BY DATE!!! #####
+    @shows = Show.all
 
-    # Combine all current shows with future shows
-    @tomorrow.each do |show|
-      @shows.push(show)
-    end
-     @dayAfter.each do |show|
-      @shows.push(show)
-    end
-     @dayAfterThat.each do |show|
-      @shows.push(show)
-    end
   end
-
 
   def new
     @show = Show.new
-    @current_user
   end
 
   def create
+
     params[:show].parse_time_select! :time
 
     # find_user --> need to hold off on this until we have User up and runnign.
     #  But we need to push a show that a user created into a user's shows []
     @show = Show.new(show_params)
 
+
+
     # create showTime w/ the other 2 params
     # parse the time and showdate
     # @show.showTime = some logic
+
     @show.showtime = Time.zone.local(@show.showdate.year,
                       @show.showdate.month,
                       @show.showdate.day,
@@ -141,14 +63,8 @@ class ShowsController < ApplicationController
   def update
 
     params[:show].parse_time_select! :time
+
     @show.update(show_params)
-    @show.showtime = Time.zone.local(@show.showdate.year,
-                      @show.showdate.month,
-                      @show.showdate.day,
-                      @show.time.hour,
-                      @show.time.min,
-                      @show.time.sec
-                      )
     if@show.save
       flash[:success] = "#{@show.title} has been updated!"
       redirect_to root_path
@@ -156,6 +72,7 @@ class ShowsController < ApplicationController
       render :edit
     end
   end
+
 
   # MODAL --> Shouldn't navigate away from index page
   def show
@@ -168,6 +85,7 @@ class ShowsController < ApplicationController
     redirect_to root_path
   end
    
+
    private
 
      def show_params
