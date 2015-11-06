29c045612d2be20bec73fbc4cf3307cbb6bade61
Mark Marsella <mmarsella114@g.rwu.edu>
2015-11-05 16:29:37 -0800

    add map to show page

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,103 +5,107 @@ class ShowsController < ApplicationController
 
 
   def index
-    # n = 2
-    # t = Time.zone.now
-    # today = t.year.to_s + "-" + t.month.to_s.rjust(n, "0") + "-" + t.day.to_s.rjust(n, "0") 
-    # threeDays = t.tomorrow.tomorrow.tomorrow
-    # endDate = threeDays.year.to_s + "-" + threeDays.month.to_s.rjust(n, "0") + "-" + threeDays.day.to_s.rjust(n, "0") 
 
-    # # API REQUEST
-    # @response = Typhoeus::Request.new("http://api.bandsintown.com/events/search.json?location=San+Francisco,CA&date=#{today},#{endDate}&radius=10&app_id=777").run
+    # session[:user_id] = nil
+    n = 2
+    t = Time.zone.now
+    today = t.year.to_s + "-" + t.month.to_s.rjust(n, "0") + "-" + t.day.to_s.rjust(n, "0") 
+    threeDays = t.tomorrow.tomorrow.tomorrow
+    endDate = threeDays.year.to_s + "-" + threeDays.month.to_s.rjust(n, "0") + "-" + threeDays.day.to_s.rjust(n, "0") 
 
-    # # API RESPONSE
-    # @response_body = JSON.parse(@response.response_body)
+    # API REQUEST
+    @response = Typhoeus::Request.new("http://api.bandsintown.com/events/search.json?location=San+Francisco,CA&date=#{today},#{endDate}&radius=10&app_id=777").run
 
-    # puts @response_body
+    # API RESPONSE
+    @response_body = JSON.parse(@response.response_body)
 
-    # # loop through the response
-    # # Make a new Show document per api response
-    # # store in the db
-    # # delete shows that have "expired"?
+    puts @response_body
 
-    # @response_body.each do |show|
+    # loop through the response
+    # Make a new Show document per api response
+    # store in the db
+    # delete shows that have "expired"?
 
-    #   # date --> gives back a DateTime object
-    #   date = DateTime.parse(show["datetime"])
+    @response_body.each do |show|
 
-    #   unless show["on_sale_datetime"] == nil
-    #   ticketDate = DateTime.parse(show["on_sale_datetime"])
-    #   end
+      # date --> gives back a DateTime object
+      date = DateTime.parse(show["datetime"])
 
-    #   artist = show["artists"][0]["name"]
+      unless show["on_sale_datetime"] == nil
+      ticketDate = DateTime.parse(show["on_sale_datetime"])
+      end
 
-    #   artistArr = artist.split(" ")
+      artist = show["artists"][0]["name"]
 
-    #   apiString = ""
+      artistArr = artist.split(" ")
 
-    #   if artistArr.length > 1
-    #     artistArr.each do |word|
-    #       if word == artistArr.last
-    #         apiString += word
-    #       else
-    #         apiString += word + "%20"
-    #       end
-    #     end
-    #   else
-    #     apiString = artist
-    #   end
+      apiString = ""
 
-    #     @artist_response = Typhoeus::Request.new("http://api.bandsintown.com/artists/#{apiString}.json?api_version=2.0&app_id=777").run
-    #     @artist_body = JSON.parse(@artist_response.response_body)
-    #     image = @artist_body["image_url"]
+      if artistArr.length > 1
+        artistArr.each do |word|
+          if word == artistArr.last
+            apiString += word
+          else
+            apiString += word + "%20"
+          end
+        end
+      else
+        apiString = artist
+      end
 
-    #   # API CALL FOR ARTIST IMAGE
-    #   # @artist_response = Typhoeus::Request.new("http://api.bandsintown.com/artists/Beach%20House.json?api_version=2.0&app_id=777").run
-    #   # @artist_body = JSON.parse(@artist_response.response_body)
-    #   # image = @artist_body["image_url"]
-    #   ##########################################################
+        @artist_response = Typhoeus::Request.new("http://api.bandsintown.com/artists/#{apiString}.json?api_version=2.0&app_id=777").run
+        
+        unless @artist_response.response_body.empty?
+          @artist_body = JSON.parse(@artist_response.response_body) 
+          image = @artist_body["image_url"]
+        end  
+      # API CALL FOR ARTIST IMAGE
+      # @artist_response = Typhoeus::Request.new("http://api.bandsintown.com/artists/Beach%20House.json?api_version=2.0&app_id=777").run
+      # @artist_body = JSON.parse(@artist_response.response_body)
+      # image = @artist_body["image_url"]
+      ##########################################################
 
-    #   venue = show["venue"]["name"]
-    #   latitude = show["venue"]["latitude"]
-    #   longitude = show["venue"]["longitude"]
-    #   tickets = show["ticket_url"]
+      venue = show["venue"]["name"]
+      latitude = show["venue"]["latitude"]
+      longitude = show["venue"]["longitude"]
+      tickets = show["ticket_url"]
 
-    #   show = Show.create(
-    #     title: artist,
-    #     venue: venue,
-    #     image_url: image,
-    #     attendance: 0,
-    #     latitude: latitude, 
-    #     longitude: longitude,
-    #     showdate: date,
-    #     saledate: ticketDate,
-    #     showtime: date
-    #     )
-    # end
-    # # IMAGE LOGIC
-    # # If image_url is present, DO NOT MAKE API CALL, if not then make API call.
-    # # Which means show = Show.new() and then .save() after api call
-    # # All the user-defined shows in the db
-    # date = Date.today
-    # # Find all shows from today til 3 days from now in DB
-    # oneDay = date.tomorrow
-    # twoDays = date.tomorrow.tomorrow
-    # threeDays = date.tomorrow.tomorrow.tomorrow
-    # @shows = Show.where('showdate': date)
-    # @tomorrow = Show.where('showdate': oneDay)
-    # @dayAfter = Show.where('showdate': twoDays)
-    # @dayAfterThat = Show.where('showdate': threeDays)
+      show = Show.create(
+        title: artist,
+        venue: venue,
+        image_url: image,
+        attendance: 0,
+        latitude: latitude, 
+        longitude: longitude,
+        showdate: date,
+        saledate: ticketDate,
+        showtime: date
+        )
+    end
+    # IMAGE LOGIC
+    # If image_url is present, DO NOT MAKE API CALL, if not then make API call.
+    # Which means show = Show.new() and then .save() after api call
+    # All the user-defined shows in the db
+    date = Date.today
+    # Find all shows from today til 3 days from now in DB
+    oneDay = date.tomorrow
+    twoDays = date.tomorrow.tomorrow
+    threeDays = date.tomorrow.tomorrow.tomorrow
+    @shows = Show.where('showdate': date)
+    @tomorrow = Show.where('showdate': oneDay)
+    @dayAfter = Show.where('showdate': twoDays)
+    @dayAfterThat = Show.where('showdate': threeDays)
 
-    # # Combine all current shows with future shows
-    # @tomorrow.each do |show|
-    #   @shows.push(show)
-    # end
-    #  @dayAfter.each do |show|
-    #   @shows.push(show)
-    # end
-    #  @dayAfterThat.each do |show|
-    #   @shows.push(show)
-    # end
+    # Combine all current shows with future shows
+    @tomorrow.each do |show|
+      @shows.push(show)
+    end
+     @dayAfter.each do |show|
+      @shows.push(show)
+    end
+     @dayAfterThat.each do |show|
+      @shows.push(show)
+    end
 
     @shows = Show.all
   end
